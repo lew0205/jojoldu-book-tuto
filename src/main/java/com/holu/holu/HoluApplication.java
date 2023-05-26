d6da86a68bca9e294b629eab5ad5810d5e4549a1
이은우 <80537533+lew0205@users.noreply.github.com>
2023-05-26 19:01:00 +0900

    Initial commit

---
+++ src/main/java/com/holu/holu/HoluApplication.java
@@ -0,0 +1,13 @@
+package com.holu.holu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoluApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoluApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/holu/holu/HoluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holu.holu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoluApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
