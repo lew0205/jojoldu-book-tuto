95758aec01318acb0416eab332c34a607d2dd004
이은우 <80537533+lew0205@users.noreply.github.com>
2023-05-30 22:34:21 +0900

    chapter 4. 머스테치로 화면 구성하기

--- src/main/java/com/holu/holu/services/posts/PostsService.java
+++ src/main/java/com/holu/holu/services/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.holu.holu.services.posts;
 
 import com.holu.holu.domain.posts.Posts;
 import com.holu.holu.domain.posts.PostsRepository;
+import com.holu.holu.web.dto.PostsListResponseDto;
 import com.holu.holu.web.dto.PostsResponseDto;
 import com.holu.holu.web.dto.PostsSaveRequestDto;
 import com.holu.holu.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,19 @@ public class PostsService {
             .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+            .map(PostsListResponseDto::new)
+            .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
