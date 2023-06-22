481bbba550e1d1e768e664c6fb7acf0370bdd26f
이은우 <80537533+lew0205@users.noreply.github.com>
2023-06-22 14:41:03 +0900

    chapter 10. 24시간 365일 중단 없는 서비스를 만들자

--- src/test/java/com/holu/holu/web/ProfileControllerTest.java
+++ src/test/java/com/holu/holu/web/ProfileControllerTest.java
@@ -13,22 +13,22 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.*;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProfileControllerTest {
 
-//    @LocalServerPort
-//    private int port;
-//
-//    @Autowired
-//    private TestRestTemplate restTemplate;
-//
-//    @Test
-//    public void profile은_인증없이_호출된다() throws Exception {
-//        String expected = "default";
-//
-//        ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
-//        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-//        assertThat(response.getBody()).isEqualTo(expected);
-//    }
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void profile은_인증없이_호출된다() throws Exception {
+        String expected = "default";
+
+        ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(expected);
+    }
 }
